b35ac28ffde2bc82b76bd4a91434533892bc0bae
DeiCarnifex <dei.carnifex@gmail.com>
2014-09-09 16:28:23 +0800

    InitialCommit

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour 
+{
+	private SpringJoint2D spring;
+	private GameObject slingshotObj;
+	private Slingshot slingshot;
+	
+	private bool dragging = false;
+
+	private Ray slingshotToAniball;
+	private float maxStretch = 3f;
+
+	private Vector3 prevVel;
+
+	void Start()
+	{
+		spring = GetComponent<SpringJoint2D>();
+		slingshotObj = GameObject.Find("slingshot");
+		slingshot = slingshotObj.GetComponent<Slingshot>();
+		slingshotToAniball = new Ray(spring.connectedBody.transform.position, Vector3.zero);
+	}
+
+	void OnMouseDown()
+	{
+		if (!rigidbody2D.isKinematic) return;
+		slingshot.currentAniball = gameObject;
+		dragging = true;
+		rigidbody2D.isKinematic = true;
+	}
+
+	void OnMouseUp()
+	{
+		dragging = false;
+		rigidbody2D.isKinematic = false;
+	}
+
+	void Update()
+	{
+		if (dragging)
+		{
+			Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			pos.z = 0f;
+			Vector3 dir = pos - spring.connectedBody.transform.position;
+			slingshotToAniball.direction = dir;
+			if (dir.magnitude < maxStretch)
+			{
+				transform.position = pos;
+			}
+			else
+			{
+				transform.position = slingshotToAniball.GetPoint(maxStretch);
+			}
+		}
+		else
+		{
+			if (!rigidbody2D.isKinematic)
+			{
+				if (rigidbody2D.velocity.magnitude < prevVel.magnitude && spring.enabled)
+				{
+					spring.enabled = false;
+					slingshot.currentAniball = null;
+					rigidbody2D.velocity = prevVel;
+				}
+				else
+				{
+					prevVel = rigidbody2D.velocity;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour 
+{
+	public GameObject currentAniball;
+
+	public LineRenderer leftRubber;
+	public LineRenderer rightRubber;
+
+	public float aniballRadius = 0.5f;
+
+	private Vector3 rubberHoldPosition;
+	private Ray slingshotToAniball;
+
+	void Start()
+	{
+		slingshotToAniball = new Ray(leftRubber.transform.position, Vector3.zero);
+		LineRendererSetup();
+	}
+
+	void LineRendererSetup()
+	{
+		leftRubber.SetPosition(0, leftRubber.transform.position);
+		rightRubber.SetPosition(0, rightRubber.transform.position);
+		leftRubber.SetPosition(1, leftRubber.transform.position);
+		rightRubber.SetPosition(1, rightRubber.transform.position);
+
+		leftRubber.sortingLayerName = "gameplay";
+		rightRubber.sortingLayerName = "gameplay";
+
+		leftRubber.sortingOrder = 2;
+		rightRubber.sortingOrder = 4;
+	}
+
+	void LineRendererUpdate()
+	{
+		Vector3 dir = currentAniball.transform.position - leftRubber.transform.position;
+		slingshotToAniball.direction = dir;
+		rubberHoldPosition = slingshotToAniball.GetPoint(dir.magnitude + aniballRadius);
+
+		leftRubber.SetPosition(1, rubberHoldPosition);
+		rightRubber.SetPosition(1, rubberHoldPosition);
+	}
+
+	void Update()
+	{
+		if (!currentAniball)
+		{
+			leftRubber.SetPosition(1, rightRubber.transform.position);
+			rightRubber.SetPosition(1, rightRubber.transform.position);
+			return;
+		}
+		LineRendererUpdate();
+	}
+}
